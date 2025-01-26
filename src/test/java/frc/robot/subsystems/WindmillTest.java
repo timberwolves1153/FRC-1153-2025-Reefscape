d440a713b12f71645bafc768bf5c8607960e49fe
Brian Gaffey <gaffey.brian@gmail.com>
2025-01-26 01:10:35 -0500

    Fix the tabbing becuase 4 spaces is life

--- src/test/java/frc/robot/subsystems/WindmillTest.java
+++ src/test/java/frc/robot/subsystems/WindmillTest.java
@@ -17,58 +17,55 @@ import org.junit.jupiter.api.Test;
 
 public class WindmillTest {
 
-  static final double DELTA = 1e-2; // acceptable deviation range
-  Windmill windmill;
-  TalonFX talonFx;
-  TalonFXSimState fx_sim;
+    static final double DELTA = 1e-2; // acceptable deviation range
+    Windmill windmill;
+    TalonFX talonFx;
+    TalonFXSimState fx_sim;
 
-  @BeforeEach // this method will run before each test
-  void setup() {
-    assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
+    @BeforeEach // this method will run before each test
+    void setup() {
+        assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
 
-    /* create the TalonFX */
-    talonFx = new TalonFX(43);
-    fx_sim = talonFx.getSimState();
-    windmill = new Windmill(new WindmillIOFX());
+        /* create the TalonFX */
+        talonFx = new TalonFX(43);
+        fx_sim = talonFx.getSimState();
+        windmill = new Windmill(new WindmillIOFX());
 
-    /* enable the robot */
-    DriverStationSim.setEnabled(true);
-    DriverStationSim.notifyNewData();
+        /* enable the robot */
+        DriverStationSim.setEnabled(true);
+        DriverStationSim.notifyNewData();
 
-    /* delay ~100ms so the devices can start up and enable */
-    Timer.delay(0.100);
-  }
+        /* delay ~100ms so the devices can start up and enable */
+        Timer.delay(0.100);
+    }
 
-  @SuppressWarnings("PMD.SignatureDeclareThrowsException")
-  @AfterEach // this method will run after each test
-  void shutdown() throws Exception {
-    windmill.close(); // destroy our intake object
-  }
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    @AfterEach // this method will run after each test
+    void shutdown() throws Exception {
+        windmill.close(); // destroy our intake object
+    }
 
-  @Test // marks this method as a test
-  void motorPower() {
-    double desiredVolts = 4;
-    fx_sim.setSupplyVoltage(RobotController.getBatteryVoltage());
+    @Test // marks this method as a test
+    void motorPower() {
+        double desiredVolts = 4;
+        fx_sim.setSupplyVoltage(RobotController.getBatteryVoltage());
 
-    StatusSignal<Double> dutyCycle = talonFx.getDutyCycle();
+        StatusSignal<Double> dutyCycle = talonFx.getDutyCycle();
 
-    /* wait for a fresh duty cycle signal */
-    dutyCycle.waitForUpdate(0.100);
-    /* verify that the motor output is zero */
-    assertEquals(dutyCycle.getValue(), 0.0, DELTA);
+        /* wait for a fresh duty cycle signal */
+        dutyCycle.waitForUpdate(0.100);
+        /* verify that the motor output is zero */
+        assertEquals(dutyCycle.getValue(), 0.0, DELTA);
 
-    // Set the windmill voltage to the desired volts
-    windmill.setVoltage(desiredVolts);
-    /* wait for the control to apply */
-    Timer.delay(0.020);
-    dutyCycle.waitForUpdate(0.100);
-    // System.out.println("DutyCycle val: " + dutyCycle.getValue());
-    /* wait for a fresh duty cycle signal */
-    // dutyCycle.waitForUpdate(0.100);
+        // Set the windmill voltage to the desired volts
+        windmill.setVoltage(desiredVolts);
+        /* wait for the control to apply */
+        Timer.delay(0.020);
+        dutyCycle.waitForUpdate(0.100);
 
-    assertEquals(
-        RobotController.getBatteryVoltage() / desiredVolts,
-        fx_sim.getMotorVoltage(),
-        DELTA); // make sure that the value set to the motor is equal to the desired voltage
-  }
+        assertEquals(
+            RobotController.getBatteryVoltage() / desiredVolts,
+            fx_sim.getMotorVoltage(),
+            DELTA); // make sure that the value set to the motor is equal to the desired voltage
+    }
 }

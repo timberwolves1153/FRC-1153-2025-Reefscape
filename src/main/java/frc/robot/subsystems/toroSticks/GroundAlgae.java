4631522bea0e93376be026998522412dcb1066a6
neil-mep <hjsm0222@gmail.com>
2025-03-28 18:40:12 -0400

    started working on ground algae

---
+++ src/main/java/frc/robot/subsystems/toroSticks/GroundAlgae.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.toroSticks;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class GroundAlgae extends SubsystemBase {}
---
+++ src/main/java/frc/robot/subsystems/toroSticks/GroundAlgaeIO.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.toroSticks;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface GroundAlgaeIO {
+
+  @AutoLog
+  public static class GroundAlgaeInputs {
+    public double pivotAppliedVolts = 0.0; // input
+    public double pivotCurrentAmps = 0.0;
+
+    public double rollerAppliedVolts = 0.0; // input
+    public double rollerCurrentAmps = 0.0;
+
+    public double pivotRotations = 0.0;
+  }
+
+  public default void updateInputs(GroundAlgaeInputs inputs) {}
+
+  public default void pivotDown() {}
+
+  public default void pivotUp() {}
+
+  public default void pivotStop() {}
+
+  public default void intake() {}
+
+  public default void stopRollers() {}
+
+  public default void outtake() {}
+
+  public default void setPivotPostion(double rotations) {}
+
+  public default void resetPivotEncoder() {}
+}

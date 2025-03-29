3d2ee89f1b0ff05fa6b139d78960a4c25f700d49
neil-mep <hjsm0222@gmail.com>
2025-03-29 12:53:42 -0400

    finished ground algae subsystem, started adding ground algae to large superstructure logic

--- src/main/java/frc/robot/subsystems/toroSticks/GroundAlgaeIOReal.java
+++ src/main/java/frc/robot/subsystems/toroSticks/GroundAlgaeIOReal.java
@@ -9,10 +9,6 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class GroundAlgaeIOReal implements GroundAlgaeIO {
 
